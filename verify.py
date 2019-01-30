2ac6758dc487a95d8574aef9dfd1d12c721bc7a6
Elsa Velazquez <elsa.velazquez@colorado.edu>
2019-01-30 13:31:49 -0700

    corrected O.x and printed matches

--- verify.py
+++ verify.py
@@ -24,53 +24,19 @@ def ecdsa_verify(msg, pk, (r, s), curve=None, hash_fn=hashlib.sha256):
     # for the algorithm.
 
 
-    
-    #BOB'S VALIDATION OF ALICE'S PUBLIC CURVE POINT, Qa
-    #####idk if we have to do this part of validating
-
-    #Qa != O
-
-    #(Ox, Oy)= isValid
-
-
-    #If x * Qa = O return True
-
-
-    #this is how O is found in ecdsa.py on line 211
-
-
-
-
+    #print Alice's public key
+    #print("Alice's public key: ", ecdsa.encode_pk(pk))
 
     #PRETEND BOB VALIDATED ALICE'S PUBLIC CURVE POINT, Qa
     #1. verification that r and s are int > 1
     #help from https://stackoverflow.com/questions/33112377/python-verifying-if-input-is-int-and-greater-than-0
-    check_r = True
-    while check_r == True:
-        #valid_r = input(r)
-        #print('This is rrrrrrrrr:   ', r)
-        try:
-          
-            if r >= 1:
-                break
-            else:
-                print("Not implemented! Amount can't be negative, try again")
-        except ValueError:
-            print("Not implemented! Amount must be a number, try again")
+    #print("r", r)
+    
 
 
-    check_s = True
-    while check_s == True:
-        #valid_r = input(r)
-        #print('This is ssssssss:   ', s)
-        try:
-          
-            if s >= 1:
-                break
-            else:
-                print("Not implemented! Amount can't be negative, try again")
-        except ValueError:
-            print("Not implemented! Amount must be a number, try again")
+    if not(0 < r < curve.n) or not(0 < s < curve.n):
+        return False
+
     
 
     #2. calculate e = hash(m)
@@ -78,12 +44,11 @@ def ecdsa_verify(msg, pk, (r, s), curve=None, hash_fn=hashlib.sha256):
     e = hash_fn(msg).hexdigest()
 
     #3. let z be the left-most bit
-    z1 = str((e)[:1])
-    z = int(ord(z1))
+    z = int(e, 16)
    
 
     #4. calculate--->   (w = s^-1 % n)
-    w = (ecdsa.modinv(s, curve.n) % curve.n)
+    w = ecdsa.modinv(s, curve.n)
 
 
     #5.  calculate u1 and u2
@@ -95,7 +60,7 @@ def ecdsa_verify(msg, pk, (r, s), curve=None, hash_fn=hashlib.sha256):
     #6. if Curve point = O it is valid
     #Find (x1, y1) by doing u1 * G + u2 *Qa
     #print("pk =     ", ecdsa.encode_pk(pk))
-    print('\n')
+    #print('\n')
     G = curve.G()   # group Generator
     #This is alice Q_a which is alice public key
     Q_a = pk
@@ -107,38 +72,13 @@ def ecdsa_verify(msg, pk, (r, s), curve=None, hash_fn=hashlib.sha256):
     #u2_Qa = u2 * Qa   #NOT YET SURE WHICH IS Qa OR HOW TO EXTRACT IT
     
     #add the calculated curve points together
-    calculated_curve_point = (u1_G.x + u2_Q_a.x, u1_G.y + u2_Q_a.y)
-
-
-    #create O to check if this is valid
-    O = Q_a.mult(curve.n)
-    #print("O-x",O.x)
-    #print("*********msg, pk, s to verify if valid", msg, pk, s)
+    calculated_curve_point = u1_G.add(u2_Q_a)
     
-
-    #conditional statement to check if signature is valid
-    print("*************r: ", r)
-    print("***********O.x:",O.x)      
-
-    #make x_1
-    x_1 = u1_G.x + u2_Q_a.x
-    print("*********  x_1:", x_1)
-
-    if r == ecdsa.modinv(x_1, curve.n):
-        print("valid signature")
-        return True
-    
-    #    print('valid signature')
-    if O == calculated_curve_point:
-        #print('Correctly rejected invalid signature')
+    if(calculated_curve_point.x == r):
         return True
     else:
         return False
-
-
-
-
-    #raise Exception('Not implemented!')
+    
 
 
 if __name__ == '__main__':
@@ -164,6 +104,17 @@ if __name__ == '__main__':
     pk = ecdsa.decode_pk(pub_key)
     # *******************************************************
     # TODO: Write your code for pairing messages to signatures here
+    i = 0
+    j = 0
+    for message in msgs:
+        for s in sigs:
+            (r_1,s_1) = ecdsa.decode_sig(s, bits=256)
+            if ecdsa_verify(message, pk, (r_1, s_1), curve=None, hash_fn=hashlib.sha256):
+                print("Match: ",message,s)
+
+
+
+
 
 
     # *******************************************************
