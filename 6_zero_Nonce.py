e1e575f281379a1d9af4fc2d81985a1b037eae66
Elsa Velazquez <elsa.velazquez@colorado.edu>
2019-02-01 01:18:55 -0700

    Create 6_zero_Nonce.py
    proof of work of 6 zero nonce

---
+++ 6_zero_Nonce.py
@@ -0,0 +1,39 @@
+#created by Elsa Velazquez
+#16.3 second Nonce with 6 leading zeros in python
+
+#referred to the following
+#https://www.youtube.com/watch?time_continue=259&v=K_Ac1Ko8-p8
+#https://www.udemy.com/build-your-blockchain-az/learn/v4/t/lecture/9657552?start=30
+
+#Group members: Josh Nguyen and Elsa Velazquez
+
+
+
+import hashlib
+import string
+
+
+def brute_force_pow():
+	proof_test = False
+	increment_nonce = 0
+	total_tests = 99999999
+	seed = 'elve5895-jong5039-'
+
+	while proof_test == False:
+		#loop to create the new nonce and test it
+		for increment_nonce in xrange(total_tests):
+			new_nonce = str(increment_nonce)
+			#print("new_nonce: ", new_nonce)
+			#while new_nonce < total_tests:
+			current_nonce = (seed + new_nonce)
+			#print('current_nonce:  ', current_nonce)
+			hashed = hashlib.sha256(current_nonce).hexdigest()
+			#print 'hashed:  ', hashed
+			if hashed.startswith('000000'):
+				print 'POW mined!  It is :', current_nonce
+				print 'Should hash to:  ', hashed
+				break
+		proof_test = True
+
+
+brute_force_pow()
